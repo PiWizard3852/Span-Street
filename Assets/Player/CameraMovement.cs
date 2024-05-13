4e79dcb553ce55a511528290d66667d067deb5d8
Owen Wood <owenwood0101@icloud.com>
2024-05-13 13:17:18 -0400

    <FEAT>(Camera): fluid motion works

--- Assets/Player/CameraMovement.cs
+++ Assets/Player/CameraMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Diagnostics;
 using UnityEngine;
 
 namespace Player
@@ -5,23 +7,38 @@ namespace Player
     public class CameraMovement : MonoBehaviour
     {
         public GameObject player;
-
+        
+        private float _targetZ;
+        private float _error;
+        private float _targetPower;
+        
         public void Start()
         {
+            var playerPosition = player.transform.position;
+            
+            transform.position = new Vector3(playerPosition.x, playerPosition.y + 10,
+                playerPosition.z - 13);
             transform.rotation = Quaternion.Euler(30, 0, 0);
-            Update();
         }
 
         public void Update()
         {
             var cameraTransform = transform;
-
             var cameraPosition = cameraTransform.position;
+
             var playerPosition = player.transform.position;
 
-            cameraPosition = new Vector3(cameraPosition.x, playerPosition.y + 10, playerPosition.z - 12);
+            _targetZ = -Math.Max(playerPosition.z - 13, cameraPosition.z + .005f);
+            _error = Math.Abs(_targetZ - cameraPosition.z);
 
+            _targetPower = playerPosition.z - 13 > cameraPosition.z + .005f ? -Math.Min(_targetZ / 50, -.01f) : .005f;
+
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            
+            cameraPosition += cameraTransform.forward * _targetPower;
             cameraTransform.position = cameraPosition;
+            
+            transform.rotation = Quaternion.Euler(30, 0, 0);
         }
     }
 }
